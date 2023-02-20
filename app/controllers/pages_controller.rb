c7a1f5b9d162b2ebed1b9a81430d5a114397fae2
liamrberney <liamrberney@gmail.com>
2023-02-20 14:30:48 -0600

    added vendors page with vendor and average_rating

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,15 @@ class PagesController < ApplicationController
   def home
   end
   def vendors
-    debugger
-    @vendors = Vendor.joins(VendorReview)
-                            .group(:name)
-                            .select('vendors.name, AVG(vendor_reviews.rating) as average_rating')
+    #debugger
+    @vendors = Vendor.select(
+    [
+      Vendor.arel_table[Arel.star], VendorReview.arel_table[:rating].average.as('average_rating')
+    ]
+    ).joins(
+      Vendor.arel_table.join(VendorReview.arel_table).on(
+        Vendor.arel_table[:id].eq(VendorReview.arel_table[:vendor_id])
+      ).join_sources
+    ).group(Vendor.arel_table[:id]).all
   end
 end
