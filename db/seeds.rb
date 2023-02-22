4154e1568df0d6701c97d4c41f71caaa37f030f9
Matan Broner <matanbroner@gmail.com>
2023-02-22 12:41:53 -0600

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "factory_bot_rails"
+
+# Create a user
+user = FactoryBot.create(:user, email: "john.doe@email.com", password: "password")
+
+# Create an entity
+entity = FactoryBot.create(:entity, name: "Entity 1")
+
+# Create a program
+program = FactoryBot.create(:program, name: "Program 1")
+
+# Create a vendor
+vendor = FactoryBot.create(:vendor, name: "Vendor 1")
+
+# Create a contract
+contract = FactoryBot.create(
+    :contract,
+    title: "Contract 1",
+    entity: entity,
+    program: program,
+    vendor: vendor,
+    point_of_contact: user,
+)
+
+# Create contract documents
+contract_document = FactoryBot.create(
+    :contract_document,
+    contract: contract,
+    file_name: "contract_doc1.pdf",
+    full_path: File.join(Rails.root, "~", "data", "contracts", "contract_document.pdf"),
+)
+
+contract_document = FactoryBot.create(
+    :contract_document,
+    contract: contract,
+    file_name: "contract_doc2.pdf",
+    full_path: File.join(Rails.root, "~", "data", "contracts", "contract_document.pdf"),
+)
+
+# Create vendor reviews
+vendor_review = FactoryBot.create(
+    :vendor_review,
+    vendor: vendor,
+    user: user,
+    rating: 5,
+    description: "This is a vendor review.",
+)
