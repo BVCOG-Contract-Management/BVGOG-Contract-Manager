a4ebf5927994e7c4812b74fd01c2ac42d0b38e17
liamrberney <liamrberney@gmail.com>
2023-02-27 22:18:46 -0600

    modified rspec again

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,13 +18,7 @@ SimpleCov.start
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
 
-  config.before(:suite) do
-    ActiveRecord::Base.connection.disconnect!
-  end
-
-  config.after(:suite) do
-    ActiveRecord::Base.establish_connection
-  end
+  
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
