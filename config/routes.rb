d86289642de06c9b8cb7f339739c1d797f6e4a2c
liamrberney <liamrberney@gmail.com>
2023-02-27 19:01:17 -0600

    modified a test

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   devise_scope :user do
-    get 'users/sign_out', to: 'devise/sessions#destroy'
+    get "users/sign_out", to: "devise/sessions#destroy"
   end
   # Example of regular route:
   # Defines the root path route ("/")
