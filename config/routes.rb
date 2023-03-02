1b951ce201742d1c7c1ab97bc628522bd8b4899b
cpepi001 <cpepi001@outlook.com>
2023-03-02 16:59:20 -0600

    fix Sign Out

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,6 @@ Rails.application.routes.draw do
   root :to => "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  devise_scope :user do
-    get 'users/sign_out', to: 'devise/sessions#destroy'
-  end
   # Example of regular route:
   # Defines the root path route ("/")
   # root "articles#index"
