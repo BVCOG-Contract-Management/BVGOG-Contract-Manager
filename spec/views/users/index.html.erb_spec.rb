cd7d76b4321a5c6c4fb0270f481ee9662e3f6a85
cpepi001 <cpepi001@outlook.com>
2023-03-09 16:03:19 -0600

    pass user tests

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -3,16 +3,15 @@ require 'rails_helper'
 RSpec.describe "users/index", type: :view do
   include FactoryBot::Syntax::Methods
   before(:each) do
-    program =  FactoryBot.create(:program) 
-    user1 = create(:user, program: program)
-    user2 = create(:user, program: program)
-    assign(:users, [
-      user1,
-      user2
-    ])
+    users = []
+    for i in 1..15
+      users << create(:user, program: create(:program, id: i))
+    end
+    @users = Kaminari.paginate_array(users).page(1).per(10)
   end
 
   it "renders a list of users" do
     render
+    assert_select "tbody>tr", count: 10
   end
 end
