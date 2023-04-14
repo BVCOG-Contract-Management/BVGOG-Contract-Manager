1b33d40f03ad92a78b22f94548704ba2950ea3f4
liamrberney <liamrberney@gmail.com>
2023-04-14 15:51:30 -0500

    added cucumber tests for users view

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -225,3 +225,10 @@ end
 Then(/^show me the page$/) do
   save_and_open_page
 end
+
+Then(/^(?:|I )should be on (.+)$/) do |page_name|
+  expected_path = path_to(page_name)
+  current_url = page.driver.browser.current_url
+  current_path = URI.parse(current_url).path
+  expect(current_path).to eq(expected_path)
+end
