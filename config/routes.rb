d275b666a4cc7bbb17bfc5ba696aa1b8048b212c
liamrberney <liamrberney@gmail.com>
2023-04-23 16:47:19 -0500

    modified send_expiry_reminder functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,14 @@ Rails.application.routes.draw do
   resources :users
   resources :contracts
 
+  post 'send_expiry_reminder', to: 'contract#send_expiry_reminder', as: 'send_expiry_reminder'
+
   # Contract Documents
   # GET
-  get "/contract_documents/:id", to: "contract_documents#download", as: "download_contract_document"
-
+  get '/contract_documents/:id', to: 'contract_documents#download', as: 'download_contract_document'
 
   # Map root path to pages/home
-  root :to => "pages#home"
+  root to: 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Example of regular route:
