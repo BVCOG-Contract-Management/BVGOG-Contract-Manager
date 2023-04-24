7486cbeba6fc70904ac6932b47a3e33cf4cc9e23
cpepi001 <cpepi001@outlook.com>
2023-04-24 18:22:37 -0500

    fix for saving reviews

--- app/controllers/vendor_reviews_controller.rb
+++ app/controllers/vendor_reviews_controller.rb
@@ -1,24 +1,36 @@
 class VendorReviewsController < ApplicationController
-    def create
-      @vendor = Vendor.find(params[:vendor_id])
-      @vendor_review = @vendor.vendor_reviews.new(vendor_review_params)
-      @vendor_review.user = current_user
-  
-      respond_to do |format|
-        if @vendor_review.save
-          format.html { redirect_to vendor_url(@vendor), notice: "Review was successfully saved." }
-          format.json { render :show, status: :created, location: @vendor }
-        else
-          # TODO: Fix this
-          format.html { render :new, status: :unprocessable_entity }
-          format.json { render json: @vendor_review.errors, status: :unprocessable_entity }
-        end
+  before_action :set_vendor, only: [:create]
+
+  def create
+    @vendor = Vendor.find(params[:vendor_id])
+    @vendor_review = @vendor.vendor_reviews.new(vendor_review_params)
+    @vendor_review.user = current_user
+
+    # Check if the user has already reviewed this vendor
+    existing_review = @vendor_review.user.vendor_reviews.find_by(vendor_id: @vendor_review.vendor_id)
+    if existing_review.present?
+      redirect_to vendor_url(@vendor), alert: "You have already reviewed this vendor."
+      return
+    end
+
+    respond_to do |format|
+      if @vendor_review.description.blank?
+        format.html { redirect_to new_vendor_vendor_review_path(@vendor), alert: "Description can't be blank." }
+        format.json { render json: { error: "Description can't be blank." }, status: :unprocessable_entity }
+      elsif @vendor_review.save
+        format.html { redirect_to vendor_url(@vendor), notice: "Review was successfully saved." }
+        format.json { render :show, status: :created, location: @vendor }
       end
     end
-  
-    private
-  
-    def vendor_review_params
-      params.require(:vendor_review).permit(:rating, :description)
-    end
   end
+
+  private
+
+  def vendor_review_params
+    params.require(:vendor_review).permit(:rating, :description)
+  end
+
+  def set_vendor
+    @vendor = Vendor.find(params[:vendor_id])
+  end
+end
