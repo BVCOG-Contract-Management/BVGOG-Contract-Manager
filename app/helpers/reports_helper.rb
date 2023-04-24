2f4adb6013208723402ab921d0801b1e95937e05
Matan Broner <matanbroner@gmail.com>
2023-04-24 12:33:46 -0500

    reports iteration 1

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,18 @@
 module ReportsHelper
+    def query_report_contracts(report)
+        # Get the contracts
+        contracts = Contract.all
+        # Filter by entity
+        contracts = contracts.where(entity_id: report.entity_id) if report.entity_id.present?
+        # Filter by program
+        contracts = contracts.where(program_id: report.program_id) if report.program_id.present?
+        # Filter by point of contact
+        contracts = contracts.where(point_of_contact_id: report.point_of_contact_id) if report.point_of_contact_id.present?
+        # Filter by expiring in days
+        if @report.expiring_in_days.present?
+          contracts = contracts.where("ends_at <= ?", report.expiring_in_days.days.from_now)
+        end
+        # Return the contracts
+        contracts
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   # GET
   get "/contract_documents/:id", to: "contract_documents#download", as: "download_contract_document"
 
+  # Download Report
+  # GET
+  get "/reports/:id/download", to: "reports#download", as: "download_report"
+
   # Map root path to pages/home
   root :to => "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
