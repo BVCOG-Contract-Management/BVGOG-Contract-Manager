1c0ac00a67be715c1bf90706984fbda96beb9e7a
Matan Broner <matanbroner@gmail.com>
2023-04-26 19:44:46 -0500

    Add check for level 3 in reports query function

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -12,6 +12,9 @@ module ReportsHelper
         if @report.expiring_in_days.present?
           contracts = contracts.where("ends_at <= ?", report.expiring_in_days.days.from_now)
         end
+        if current_user.level == UserLevel::THREE
+          contracts = contracts.where(entity_id: current_user.entities.pluck(:id))
+        end
         # Return the contracts
         contracts
     end
