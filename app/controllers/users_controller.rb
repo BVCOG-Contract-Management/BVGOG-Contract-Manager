5d9c4e96bfae0065773eb996902317e07d7fdc9e
Matan Broner <matanbroner@gmail.com>
2023-04-26 21:09:40 -0500

    fix sign out button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -88,7 +88,6 @@ class UsersController < ApplicationController
       format.json { head :no_content }
     end
   end
-
   private
 
   # Use callbacks to share common setup or constraints between actions.
