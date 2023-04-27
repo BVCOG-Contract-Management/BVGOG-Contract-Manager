c5acd7d0d2b915eed9508de94b7429f3a1ed60be
Matan Broner <matanbroner@gmail.com>
2023-04-27 10:42:13 -0500

    remove mata user_entities table

--- db/migrate/20230425014920_create_users_entities_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsersEntitiesJoinTable < ActiveRecord::Migration[7.0]
-  def change
-    create_join_table :users, :entities do |t|
-      t.index :user_id
-      t.index :entity_id
-    end
-  end
-end
