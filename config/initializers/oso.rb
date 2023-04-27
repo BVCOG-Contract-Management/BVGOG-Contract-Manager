ac995d727e82a3f38db39efa7b060d8558d2661e
Matan Broner <matanbroner@gmail.com>
2023-04-26 21:11:50 -0500

    fix oso duplicate class alias error

--- config/initializers/oso.rb
+++ config/initializers/oso.rb
@@ -1,7 +1,15 @@
 OSO = Oso.new
 ActiveSupport::Reloader.to_prepare do
-    OSO.register_class(User, fields: {id: :integer})
-    OSO.register_class(Contract, fields: {id: :integer})
+    begin
+        OSO.register_class(User, fields: {id: :integer})
+        OSO.register_class(Contract, fields: {id: :integer})
+    rescue => e
+        if e.class == Oso::DuplicateClassAliasError
+            # Do nothing
+        else
+            raise e
+        end
+    end
 
     OSO.load_files(["app/rbac/main.polar"])
 end
