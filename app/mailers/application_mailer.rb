f8e58fd716de08101a4989b83d7af9d1b90ee32d
Matan Broner <matanbroner@gmail.com>
2023-04-28 16:23:44 -0500

    default from

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,3 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "matan@matanbroner.com"
   layout "mailer"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,9 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  
+
   # SendGrid
+  ActionMailer::Base.default :from => 'matanbroner@gmail.com'
   ActionMailer::Base.smtp_settings = {
     :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
     :password => Rails.application.credentials.sendgrid_api_key, # This is the secret sendgrid API key which was issued during API key creation
