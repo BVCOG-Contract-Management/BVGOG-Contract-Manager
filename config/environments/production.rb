c704d6cbfe92dce289e26d03d8d43b112a635c6a
Matan Broner <matanbroner@gmail.com>
2023-04-28 16:36:22 -0500

    config based mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
     :password => Rails.application.credentials.sendgrid_api_key, # This is the secret sendgrid API key which was issued during API key creation
-    :domain => 'matanbroner.com',
-    :address => 'smtp.sendgrid.net',
+    :domain => ENV['MAIL_DOMAIN'],
+    :address => ENV['MAIL_ADDRESS'],
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
