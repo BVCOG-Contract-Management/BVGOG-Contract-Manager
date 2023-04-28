f83602f4b2fa5304647bcb9d1921ab4cde55408d
Matan Broner <matanbroner@gmail.com>
2023-04-28 15:42:14 -0500

    add host to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Default host
+  Rails.application.routes.default_url_options[:host] = ENV.fetch("HOST")
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
