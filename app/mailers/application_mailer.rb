c67a4f1c6175e088bc50d9746122302338b1f96b
Matan Broner <matanbroner@gmail.com>
2023-04-28 21:36:48 -0500

    updated mailer templates, custom devise mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
   layout "mailer"
+
+  # Set attachments for all emails
+  before_action :add_inline_attachments!
+
+  private
+
+  # Set attachments for all emails
+  
+  def add_inline_attachments!
+    attachments.inline["bvcog-logo.png"] = File.read("#{Rails.root}/app/assets/images/bvcog-logo.png")
+  end
 end
