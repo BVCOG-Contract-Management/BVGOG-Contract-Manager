42d32d4453304a64d53d10655008831f4c75d9e4
Matan Broner <matanbroner@gmail.com>
2023-04-29 21:47:19 -0500

    config user migration

--- db/migrate/20230430003137_create_bvcog_configs_users_join_table.rb
+++ db/migrate/20230430003137_create_bvcog_configs_users_join_table.rb
@@ -1,6 +1,6 @@
 class CreateBvcogConfigsUsersJoinTable < ActiveRecord::Migration[7.0]
   def change
-    create_table :bvcog_configs_users, id: false do |t|
+    create_table :bvcog_configs_users do |t|
       t.belongs_to :bvcog_config
       t.belongs_to :user
     end
