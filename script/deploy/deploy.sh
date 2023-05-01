b2a80558e14a4a75899bf5c6d87061517b50dcc4
Matan Broner <matanbroner@gmail.com>
2023-04-30 23:57:33 -0500

    update deploy script

--- script/deploy/deploy.sh
+++ script/deploy/deploy.sh
@@ -8,11 +8,11 @@ set -e
 # Set environment variables
 
 # General settings
-export HOST=localhost:3000
+export HOST=54.173.238.151:3000
 export LANG=en_US.UTF-8
 
 # Database settings
-export DATABASE_URL=postgres://localhost:5432/postgres
+export DATABASE_URL=postgres://localhost:5432/bvcog
 
 # Rails environment
 export RAILS_ENV=production
@@ -28,13 +28,21 @@ export MAIL_DEFAULT_FROM=matan@matanbroner.com
 export MAIL_DOMAIN=matanbroner.com
 export MAIL_USERNAME=apikey
 
-# Run database migrations
-bundle exec rake db:create db:migrate db:seed
+# Install dependencies
+bundle install
+
+# Install PSQL client
+apt-get install libpq-dev
+
+# Run database setup if no --skip-db-setup flag is passed
+if [ "$1" != "--skip-db-setup" ]; then
+  bundle exec rake db:create db:migrate db:seed
+fi
 
 # Precompile assets
 bundle exec rake assets:precompile
 
 # Start the server
-rails s
+rvmsudo rails s -p 80
 
 
