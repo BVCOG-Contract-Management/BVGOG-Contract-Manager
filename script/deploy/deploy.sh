8fb8361dfc233d3d21298c071e8a9bcbd9de2e6b
Matan Broner <matanbroner@gmail.com>
2023-05-01 00:53:38 -0500

    deploy script

--- script/deploy/deploy.sh
+++ script/deploy/deploy.sh
@@ -58,11 +58,6 @@ bundle exec whenever --clear-crontab
 # Use whenever to update the crontab
 bundle exec whenever --update-crontab
 
-# Kill the server if it's running
-if [ -f tmp/pids/server.pid ]; then
-  kill -9 $(cat tmp/pids/server.pid)
-fi
-
 # Start the server in the background
 bundle exec rails server --daemon
 
