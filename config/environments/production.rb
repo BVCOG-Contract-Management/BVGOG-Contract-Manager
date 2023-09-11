99c809fd36ff13c08a75cf8b907a602ea942e076
Huy Lai <lai.huy@tamu.edu>
2023-09-11 10:55:42 -0500

    host hot fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Default host
-  Rails.application.routes.default_url_options[:host] = ENV.fetch("HOST")
+  # Rails.application.routes.default_url_options[:host] = ENV.fetch("HOST")
 
   # Code is not reloaded between requests.
   config.cache_classes = true
