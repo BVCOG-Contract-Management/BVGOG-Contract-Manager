b0c512e61434c043ba130ed1ff613cdd7a34184c
Huy Lai <lai.huy@tamu.edu>
2023-10-04 15:16:59 -0500

    add some regex

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -1,26 +1,22 @@
-Given('I am on the sign_in page') do
-    visit '/users/sign_in'
-end
-
-Given('I am on the users page') do
-    visit '/users'
-end
-
-When(/^I follow "([^"]*)"$/) do |link|
-    click_link(link)
-end
-
-Then('I should be on the admin page') do
-    expected_path = '/admin'
-	current_url = page.driver.browser.current_url
-	current_path = URI.parse(current_url).path
-	expect(current_path).to eq(expected_path)
+Given(/^I am on (.+)$/) do |page_name|
+	visit path_to(page_name)
 end
 
 When(/^I fill in "([^"]*)" with "([^"]*)"$/) do |field, value|
-    fill_in(field, with: value)
+	fill_in(field, with: value)
 end
 
 When(/^I press "([^"]*)"$/) do |button|
-    click_button(button)
+	click_button(button)
+end
+
+When(/^I follow "([^"]*)"$/) do |link|
+	click_link(link)
+end
+
+Then(/^I should be on (.+)$/) do |page_name|
+	expected_path = path_to(page_name)
+	current_url = page.driver.browser.current_url
+	current_path = URI.parse(current_url).path
+	expect(current_path).to eq(expected_path)
 end
