6e0a461aa9f01fff0859b88b3f2102a5c75baa00
Huy Lai <lai.huy@tamu.edu>
2023-10-06 14:32:23 -0500

    fix contract_viewing.feature

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -58,3 +58,21 @@ Then(%r{^I should see /([^/]*)/$}) do |regexp|
 		assert page.has_xpath?('//*', text: regexp)
 	end
 end
+
+Then(/^I should not see "([^"]*)"$/) do |text|
+	if page.respond_to? :should
+		page.should have_no_content(text)
+	else
+		assert page.has_no_content?(text)
+	end
+end
+
+Then(%r{^I should not see /([^/]*)/$}) do |regexp|
+	regexp = Regexp.new(regexp)
+
+	if page.respond_to? :should
+		page.should have_no_xpath('//*', text: regexp)
+	else
+		assert page.has_no_xpath?('//*', text: regexp)
+	end
+end
